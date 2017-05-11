7de7a2b9e10769753e2eb2ed77b22f1ad2d524c0
jliu <534836006>
2017-05-11 14:38:38 +0800

    issue:add consul

--- client/src/main/java/com/hulk/client/service/ComputeClient.java
+++ client/src/main/java/com/hulk/client/service/ComputeClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("service")
+@FeignClient("test-service")
 public interface ComputeClient {
     @RequestMapping(method = RequestMethod.GET, value = "/add")
     Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
